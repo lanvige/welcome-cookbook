b4a71ad530637c90497d112986503c361d0d50a6
Lanvige Jiang <lanvige@gmail.com>
2014-12-11 22:28:09 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Welcome chef cookbook
+
+Recipe used for configuring basic look and feel of a fresh Ubuntu
+server. In particular:
+
+- Add an ASCI art banner on login to production environments
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name              "welcome-cookbook"
+maintainer        "Lanvige Jiang"
+maintainer_email  "lanvige@gmail.com"
+description       "Installs tools to make the server nicer to work on, quite opinionated"
+version           "0.1.0"
+
+recipe "welcome-cookbook", "Adds visual flag to production environment as well as htop, vim and zip, also allows for the installation of additional locales"
+
+supports "ubuntu"
+
+depends "locales"
