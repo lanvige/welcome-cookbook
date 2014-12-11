81f8eb8daa214f8f88de43206c94428126cc693e
Lanvige Jiang <lanvige@gmail.com>
2014-12-11 22:51:07 +0800

    update version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "welcome-cookbook"
 maintainer        "Lanvige Jiang"
 maintainer_email  "lanvige@gmail.com"
 description       "Installs tools to make the server nicer to work on, quite opinionated"
-version           "0.1.0"
+version           "0.1.1"
 
 recipe "welcome-cookbook", "Adds visual flag to production environment as well as htop, vim and zip, also allows for the installation of additional locales"
 
